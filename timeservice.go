5f4e98a82603ae3a70144e76b4304039466d8601
云舒 <wangming.xie@lbk.one>
2024-04-18 15:16:52 +0800

    feat: add timeservice

--- timeservice.go
+++ timeservice.go
@@ -1,1 +1,37 @@
 package GoUtils
+
+import "time"
+
+// NowFmtStr 返回格式化的时间
+func NowFmtStr() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+// TimeDiff 2个字符串时间差值
+func TimeDiff(start, end string) int64 {
+	startTime, _ := time.ParseInLocation("2006-01-02 15:04:05", start, time.Local)
+	endTime, _ := time.ParseInLocation("2006-01-02 15:04:05", end, time.Local)
+	return endTime.Unix() - startTime.Unix()
+}
+
+// TimeStampS 获取时间戳-秒
+func TimeStampS() int64 {
+	return time.Now().Unix()
+}
+
+// TimeStampMs 获取时间戳-毫秒
+func TimeStampMs() int64 {
+	return time.Now().UnixNano() / 1e6
+}
+
+// TimeStamp2FmtStr 时间戳转为格式化时间
+func TimeStamp2FmtStr(timeStamp int64) string {
+	timeObj := time.Unix(timeStamp, 0)
+	return timeObj.Format("2006-01-02 15:04:05")
+}
+
+// FmtStr2TimeStamp 格式化时间转为时间戳
+func FmtStr2TimeStamp(timeStr string) int64 {
+	timeObj, _ := time.ParseInLocation("2006-01-02 15:04:05", timeStr, time.Local)
+	return timeObj.Unix()
+}
---
+++ utils_test.go
@@ -0,0 +1,30 @@
+package GoUtils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTimeDiff(t *testing.T) {
+	fmt.Println(TimeDiff("2020-01-01 10:00:00", "2020-01-01 11:00:00"))
+}
+
+func TestTimeStampMs(t *testing.T) {
+	fmt.Println(TimeStampMs())
+}
+
+func TestTimeStampS(t *testing.T) {
+	fmt.Println(TimeStampS())
+}
+
+func TestNowFmtStr(t *testing.T) {
+	fmt.Println(NowFmtStr())
+}
+
+func TestTimeStamp2FmtStr(t *testing.T) {
+	fmt.Println(TimeStamp2FmtStr(1713424488))
+}
+
+func TestFmtStr2TimeStamp(t *testing.T) {
+	fmt.Println(FmtStr2TimeStamp("2020-01-01 10:00:00"))
+}
